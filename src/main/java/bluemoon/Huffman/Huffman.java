3e4f3d709981c3dc4e63fd7f56d797127bf2ca32
Anssi Kinnunen <aatkin@utu.fi>
2013-02-01 20:33:19 +0200

    update

--- src/main/java/bluemoon/Huffman/Huffman.java
+++ src/main/java/bluemoon/Huffman/Huffman.java
@@ -1,13 +1,8 @@
 package bluemoon.Huffman;
 
-
 public class Huffman {
 
     public static void main(String[] args) {
-	String[] s = "asdasdasd".split("");
 
-	for (int i = 0; i < s.length - 1; i++) {
-	    System.out.print(s[i] + " ");
-	}
     }
 }
