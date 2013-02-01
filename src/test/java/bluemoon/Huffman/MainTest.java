b8f1495cfbb2e30376c8e5f01ea54ba639a4faca
Anssi Kinnunen <aatkin@utu.fi>
2013-02-01 18:16:36 +0200

    commit saatana

---
+++ src/test/java/bluemoon/Huffman/MainTest.java
@@ -0,0 +1,14 @@
+package bluemoon.Huffman;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+	fail("Not yet implemented");
+    }
+
+}
