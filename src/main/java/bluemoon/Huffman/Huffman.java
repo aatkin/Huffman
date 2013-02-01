6af75e1f784153dad63240cdf8e25c4d08d8cb9e
Anssi Kinnunen <aatkin@utu.fi>
2013-02-01 20:24:19 +0200

    main class for the prog, empty for the mom

---
+++ src/main/java/bluemoon/Huffman/Huffman.java
@@ -0,0 +1,13 @@
+package bluemoon.Huffman;
+
+
+public class Huffman {
+
+    public static void main(String[] args) {
+	String[] s = "asdasdasd".split("");
+
+	for (int i = 0; i < s.length - 1; i++) {
+	    System.out.print(s[i] + " ");
+	}
+    }
+}
